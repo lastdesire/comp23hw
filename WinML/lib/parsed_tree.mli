854776c3844e11609a6634420f905a9da61efd0a
lastdesire <deadinsidezz123450@mail.ru>
2023-10-23 03:30:18 +0300

    Updated parser with interface for tests

--- WinML/lib/parsed_tree.mli
+++ WinML/lib/parsed_tree.mli
@@ -1,26 +1,12 @@
 exception Parsed_tree_error of string
-
-let parsed_tree_error s = raise (Parsed_tree_error s)
-
-type constant =
-  | CInt of int
-
-type binop = 
-  | Add (** + *)
-  | Sub (** - *)
-  | Mul (** * *)
-
-  | Eq (** = *)
-
+val parsed_tree_error : string -> 'a
+type constant = CInt of int
+type binop = Add | Sub | Mul | Eq
 type expression =
-  | Const of constant
+    Const of constant
   | Var of string
   | BinOp of binop * expression * expression
   | IfThenElse of expression * expression * expression
   | Application of expression * expression * expression list
-
 type declaration = string * string list * expression
-
-type func =
-  | Function of declaration
-  | RecFunction of declaration
+type func = Function of declaration | RecFunction of declaration
